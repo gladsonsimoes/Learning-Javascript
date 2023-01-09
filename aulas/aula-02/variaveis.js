6558e7017ca4e9a699d276064ffb5d76d23616d2
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-01-09 16:27:48 -0300

    Add files via upload

---
+++ aulas/aula-02/variaveis.js
@@ -0,0 +1,34 @@
+//o Javascript faz parte de uma linguagem fortemente tipada e dinamicamente tipada
+//ou seja a linguagem pode definir uma variavel "global" que pode ser texto, numero, booleano e etc.
+
+var idade = 0;
+var nome = "Gladson";
+console.log(idade); //printar no console
+document.write("<h1>Olá " + nome + "</h1>"); //printar no site
+
+//soma
+var soma;
+var n1 = 10;
+var n2 = 2;
+soma = n1 + n2;
+console.log(soma);
+
+//Diferenca de variavel
+//Em Javascript as Variaveis com Letra maiscula e minuscula são totalmente diferentes veja:
+
+var nome1 = "Ramon";
+var nome2 = "Caio";
+var Nome2 = "Roberto";
+console.log(nome2);
+
+//Quebra de Linha
+//Usamos a tag <br> do html para quebra de linha
+
+var nome1 = "Ramon";
+var nome2 = "Roberto";
+
+document.write(nome1 + "<br>");
+document.write(nome2 + "<br>");
+
+
+
